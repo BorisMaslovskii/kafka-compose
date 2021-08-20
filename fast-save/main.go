14afd3deb83958b61307df35f1c637610dfa4e75
Boris Maslovskiy <boris.maslovskiy@bergen.tech>
2021-08-20 18:14:49 +0300

    fast transmition started

---
+++ fast-save/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:        []string{"localhost:9092"},
+		Topic:          "fast",
+		GroupID:        "fastread-group",
+		CommitInterval: 10 * time.Second,
+	})
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(r *kafka.Reader) {
+		<-c
+		cleanup(r)
+		os.Exit(1)
+	}(r)
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		log.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+}
+
+func cleanup(r *kafka.Reader) {
+	log.Println("writer closed")
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+
+	log.Println("reader closed")
+}
---
+++ fast-send/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+
+	// make a writer that produces to topic-A, using the least-bytes distribution
+	w := &kafka.Writer{
+		Addr:         kafka.TCP("localhost:9092"),
+		Topic:        "fast",
+		Balancer:     &kafka.LeastBytes{},
+		BatchTimeout: 500 * time.Millisecond,
+		BatchSize:    10000,
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(w *kafka.Writer) {
+		<-c
+		cleanup(w)
+		os.Exit(1)
+	}(w)
+
+	startTime := time.Now()
+	wg := sync.WaitGroup{}
+	messages := make([]kafka.Message, 0, 10000)
+	for i := 1; i <= 10000; i++ {
+		messages = append(messages,
+			kafka.Message{
+				Key:   []byte(fmt.Sprint(i)),
+				Value: []byte("FastTest"),
+			},
+		)
+	}
+	wg.Wait()
+	log.Print(time.Since(startTime))
+	log.Print(len(messages))
+	err := w.WriteMessages(context.Background(), messages...)
+	if err != nil {
+		log.Print("failed to write messages:", err)
+	}
+	log.Print(time.Since(startTime))
+
+}
+
+func cleanup(w *kafka.Writer) {
+
+	if err := w.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+	log.Println("writer closed")
+}
